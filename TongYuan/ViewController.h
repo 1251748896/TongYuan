08fc43e48aacad43969702ce67ee510c9633c958
姜波 <jiangbo@jiangbodeMacBook-Air.local>
2017-12-06 19:22:51 +0800

    Initial Commit

---
+++ TongYuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TongYuan
+//
+//  Created by 姜波 on 2017/12/6.
+//  Copyright © 2017年 姜波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TongYuan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TongYuan
+//
+//  Created by 姜波 on 2017/12/6.
+//  Copyright © 2017年 姜波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
